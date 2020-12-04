fdd310667c6db90d1fa52b38a94beee725f528cf
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-04 14:45:22 +0330

    Update Problem Set 0 - 1.java

--- Problem Set 0 - 1.java
+++ Problem Set 0 - 1.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Hello {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine(); // We take the input.
