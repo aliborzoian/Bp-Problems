690117fa3c93c3d199f55d29c1937f2c15656cc5
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-04 14:47:55 +0330

    Update Problem Set 0 - 6.java

--- Problem Set 0 - 6.java
+++ Problem Set 0 - 6.java
@@ -4,7 +4,7 @@
 import javax.swing.*;
 import java.util.Scanner;
 
-public class Hello {
+public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
