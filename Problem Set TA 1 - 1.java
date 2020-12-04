3ee86ecdc51812f44a1f5aa74a85b9127328d369
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-04 13:46:41 +0330

    Create Problem Set TA 1 - 1.java

---
+++ Problem Set TA 1 - 1.java
@@ -0,0 +1,24 @@
+// Problem: Write a code that converts number n from base 10 to base 2.
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int Number = scanner.nextInt();
+        
+        StringBuilder Answer = new StringBuilder();
+
+        while(Number > 0){
+            int temp = Number % 2; // Typical base 10 to base 2 convertor. We just divide Number by 2 until it hits 0 and put the reminders in a reversed order.
+            Number = Number / 2;
+            Answer.append(String.valueOf(temp));
+        }
+        
+        StringBuilder Final_Answer = Answer.reverse(); // Reversing Answer since the reminders mustt be written in a reversed order.
+        System.out.println(Final_Answer);
+
+    }
+}
