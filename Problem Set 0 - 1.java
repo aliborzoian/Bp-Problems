9919a67ed33c1291d35bbb7a8219d6be04520625
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-04 12:52:40 +0330

    Create Problem Set 0 - 1.java

---
+++ Problem Set 0 - 1.java
@@ -0,0 +1,11 @@
+// Problem: Write a problem that takes an string input from the user and replies it with "Hello + string".
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine(); // We take the input.
+        System.out.println("Hello " + str); // We add a space after "Hello" since we want an output like "Hello Sina" instead of "HelloSina" for the input "Sina".
+    }
+}
