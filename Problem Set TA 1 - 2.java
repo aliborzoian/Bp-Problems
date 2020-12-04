c4b4d09726065b405e125d696c2ebd6cf4947e21
Sina Ghaseminejad <sina.qane@yahoo.com>
2020-12-04 14:44:30 +0330

    Update Problem Set TA 1 - 2.java

--- Problem Set TA 1 - 2.java
+++ Problem Set TA 1 - 2.java
@@ -10,7 +10,7 @@ public class Main {
         long[] A = new long[n];
         
         int i = 0;
-        while(i < n){
+        while(i < n) {
             A[i] = scanner.nextLong(); // Saving all numbers in an array
             i++;
         }
@@ -18,18 +18,18 @@ public class Main {
         int max = 1; // Since every number itself is an ascending subsequence.
         int counter = 1;
         int j = 0;
-        while (j < n - 1){
+        while (j < n - 1) {
             if (A[j] < A[j+1]){
                 counter++; // The counter variable counts the number of terms in an ascending subsequence for us. If we reach a number than makes our subsequence descending again, it stops counting.
             } else {
-                if (max < counter){
+                if (max < counter) {
                     max = counter; // We check if our counter is greater than the max length of ascending subsequences so far or not. If yes, max = counter, and we continue. Otherwise, counter just begins counting again.
                 }
                 counter = 1;
             }
             j++;
         }
-        if (max < counter){
+        if (max < counter) {
             max = counter;
         }
         System.out.println(max);
